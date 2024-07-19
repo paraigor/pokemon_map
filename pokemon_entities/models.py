570929b3183f0b79149cf92ddba128c9a8011664
paraigor <103633539+paraigor@users.noreply.github.com>
2024-07-19 22:23:32 +0300

    Add verbose names to all fields of models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,17 +2,22 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    title_ru = models.CharField("название", max_length=200)
+    title_en = models.CharField(
+        "название на английском", max_length=200, blank=True
+    )
+    title_jp = models.CharField(
+        "название на японском", max_length=200, blank=True
+    )
+    image = models.ImageField("изображение", null=True, blank=True)
+    description = models.TextField("описание", null=True, blank=True)
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name="next_evolution"
+        related_name="next_evolution",
+        verbose_name="из кого эволюционирует",
     )
 
     def __str__(self):
@@ -20,16 +25,18 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField(null=True, blank=True)
-    lon = models.FloatField(null=True, blank=True)
-    appeared_at = models.DateTimeField(null=True, blank=True)
-    disappeared_at = models.DateTimeField(null=True, blank=True)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    pokemon = models.ForeignKey(
+        Pokemon, on_delete=models.CASCADE, verbose_name="покемон"
+    )
+    lat = models.FloatField("широта", null=True, blank=True)
+    lon = models.FloatField("долгота", null=True, blank=True)
+    appeared_at = models.DateTimeField("время появления", null=True, blank=True)
+    disappeared_at = models.DateTimeField("время исчезновения", null=True, blank=True)
+    level = models.IntegerField("уровень", default=0)
+    health = models.IntegerField("здоровье", default=0)
+    strength = models.IntegerField("сила", default=0)
+    defence = models.IntegerField("защита", default=0)
+    stamina = models.IntegerField("выносливость", default=0)
 
     def __str__(self):
         return f"{self.pokemon.title_ru} entity"
